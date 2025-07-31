b303a2f0dd3ebb3559e93b229490a8f8319329d8
bilalakmal-2004 <ghulambeast468@gmail.com>
2025-08-01 03:01:56 +0500

    Local Host stuff changes for the deployment.

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,15 @@ import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://smit-hackathon-client-git-main-bilal-akmals-projects.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
