34a24734555f08d9b2df25ce85b5576734b1d946
bilalakmal-2004 <ghulambeast468@gmail.com>
2025-11-29 01:15:58 +0500

    Comments added.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,3 +1,7 @@
+//main entry point of the React application
+// It sets up the root component and wraps it with a ThemeProvider for theming context
+// Also includes global styles from index.css
+// Uses React's StrictMode for highlighting potential problems in the application
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
